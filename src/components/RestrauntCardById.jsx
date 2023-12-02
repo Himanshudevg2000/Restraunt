9bd9b384d48c30a9596e2ea4aed9148208eea766
your.user.name <himanshudevgade87@gmail.com>
2023-12-02 16:43:36 +0530

    update

--- src/components/RestrauntCardById.jsx
+++ src/components/RestrauntCardById.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
 import useRestrauntMenu from "../utils/useRestrauntMenu";
+import RestrauntCategory from "./RestrauntCategory";
 
 const RestrauntCardById = () => {
   const { id } = useParams();
@@ -16,25 +17,25 @@ const RestrauntCardById = () => {
         "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory"
     );
 
-  console.log("categories: ", categories);
+  // console.log("categories: ", categories);
 
-  const name = restrauntName?.data?.cards[0]?.card?.card?.info.name;
-  const costForTwoMessage = restrauntName?.data?.cards[0]?.card?.card?.info.costForTwoMessage;
-  const cuisines = restrauntName?.data?.cards[0]?.card?.card?.info.cuisines;
-  const avgRating = restrauntName?.data?.cards[0]?.card?.card?.info.avgRating
+  const restrauntdata = restrauntName?.data?.cards[0]?.card?.card?.info;
 
   return (
-    <div className=" mx-60 my-5 p-14 bg-gray-100 items-center text-center">
-      <h1 className=" font-bold text-xl ">
-        {name}
+    <div className=" mx-60 my-5 p-14 items-center text-center">
+      <h1 className=" font-bold text-3xl ">
+        {restrauntdata.name}
       </h1>
-      <h3 className="font-bold text-l">
-        {cuisines.join(", ")} - {costForTwoMessage}
+      <h3 className="font-bold text-xl">
+        {restrauntdata.cuisines.join(", ")} - {restrauntdata.costForTwoMessage}
       </h3>
-      <h3>{avgRating}</h3>
+      <h3 className="font-bold text-ll">{restrauntdata.avgRating}-Rating</h3>
 
-      <h2 className="font-bold text-3xl">Menu</h2>
-      {categories.map(()=> { })}
+      <h2 className="font-bold text-xl">Menu</h2>
+      {categories.map((category) => (
+        <RestrauntCategory resdata={category?.card?.card} />
+      ))}
+
     </div>
   );
 };
--- src/components/RestrauntCategory.jsx
+++ src/components/RestrauntCategory.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
-const RestrauntCategory = () => {
+const RestrauntCategory = ({resdata}) => {
   return (
-    <div>RestrauntCategory</div>
+    // <div className='m-2 px-2 py-6 bg-zinc-200'>
+    <div className=' mx-auto my-4 p-4 bg-zinc-50 shadow-lg flex justify-between'>
+      <span>{resdata.title} </span>
+      <span>◀</span>
+    </div>
+
   )
 }
 
