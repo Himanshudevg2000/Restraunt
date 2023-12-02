ca97e02872314039df8bd994c36164af352ba19e
Himanshudevg2000 <himanshudevgade87@gmail.com>
2023-12-02 07:24:46 +0530

    update

--- src/components/RestrauntCardById.jsx
+++ src/components/RestrauntCardById.jsx
@@ -13,86 +13,28 @@ const RestrauntCardById = () => {
     restrauntName?.data?.cards?.[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
-        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+        "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory"
     );
 
   console.log("categories: ", categories);
 
+  const name = restrauntName?.data?.cards[0]?.card?.card?.info.name;
+  const costForTwoMessage = restrauntName?.data?.cards[0]?.card?.card?.info.costForTwoMessage;
+  const cuisines = restrauntName?.data?.cards[0]?.card?.card?.info.cuisines;
+  const avgRating = restrauntName?.data?.cards[0]?.card?.card?.info.avgRating
+
   return (
-    <div className=" m-4 p-14 w-[450px] bg-gray-100 items-center text-center">
-      <h1 className=" font-bold ">
-        {restrauntName?.data?.cards[0]?.card?.card?.info.name}
+    <div className=" mx-60 my-5 p-14 bg-gray-100 items-center text-center">
+      <h1 className=" font-bold text-xl ">
+        {name}
       </h1>
-      <h3 className="text-red-600">
-        {restrauntName?.data?.cards[0]?.card?.card?.info.costForTwoMessage}
+      <h3 className="font-bold text-l">
+        {cuisines.join(", ")} - {costForTwoMessage}
       </h3>
-      <h3>{restrauntName?.data?.cards[0]?.card?.card?.info.avgRating}</h3>
+      <h3>{avgRating}</h3>
 
       <h2 className="font-bold text-3xl">Menu</h2>
-
-      {<h2 className=" font-bold text-2xl">Best seller Items</h2>}
-      <ul>
-        {(
-          restrauntName?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
-            (c) =>
-              c?.card?.cards?.["@type"] ===
-              "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-          ) || []
-        ).map((card, index) => (
-          <li key={index}>
-            {card?.card?.card?.itemCards?.[0]?.card?.info?.name} -{" "}
-            {(card?.card?.card?.itemCards?.[0]?.card?.info?.price || 0) / 100}{" "}
-            Rs
-          </li>
-        ))}
-      </ul>
-
-      {/* {<h2 className=" font-bold text-2xl">Best seller Items</h2>}
-      <ul>
-        {restrauntName?.data?.cards[1]?.groupedCard?.cardGroupMap?.REGULAR?.cards ||
-        (
-          restrauntName?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR
-            ?.cards ||
-          restrauntName?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR
-            ?.cards ||
-          []
-        ).map((card, index) => (
-          <li key={index}>
-            {card?.card?.card?.itemCards?.[0]?.card?.info?.name} -{" "}
-            {(card?.card?.card?.itemCards?.[0]?.card?.info?.price || 0) / 100}{" "}
-            Rs
-          </li>
-        ))}
-      </ul> */}
-
-      {/* {<h2 className=" font-bold text-2xl">Recommended Items </h2>}
-      <ul>
-        {
-        (
-          restrauntName?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR
-            ?.cards ||
-          []
-        ).map((card, index) => (
-          <li key={index}>
-            
-            {card?.card?.card?.itemCards?.[2]?.card?.info?.name}
-          </li>
-        ))}
-      </ul> */}
-
-      {/* {<h2 className=" font-bold text-2xl">Party Packs Items</h2>}
-      <ul>
-        {
-        (
-          restrauntName?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR
-            ?.cards ||
-          []
-        ).map((card, index) => (
-          <li key={index}>
-            {card?.card?.card?.itemCards?.[3]?.card?.info?.name}
-          </li>
-        ))}
-      </ul> */}
+      {categories.map(()=> { })}
     </div>
   );
 };
---
+++ src/components/RestrauntCategory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RestrauntCategory = () => {
+  return (
+    <div>RestrauntCategory</div>
+  )
+}
+
+export default RestrauntCategory;
